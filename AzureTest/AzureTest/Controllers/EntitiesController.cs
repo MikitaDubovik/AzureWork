66b23e0083e1e66cd099d02be5a441261309df60
Mikita Dubovik <Mikita_Dubovik@epam.com>
2018-11-01 14:48:44 +0300

    Get

---
+++ AzureTest/AzureTest/Controllers/EntitiesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using AzureWork.DbModel;
+using AzureWork.Service;
+
+namespace AzureWork.Controllers
+{
+    [RoutePrefix("entities")]
+    public class EntitiesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            ProductService service = new ProductService();
+
+            return service.Get();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        // GET api/values/5
+        public Product GetEntity(int id)
+        {
+            ProductService service = new ProductService();
+
+            return service.Get(id);
+        }
+
+        [HttpPost]
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        [HttpPut]
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete]
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AzureTest/AzureWork.Service/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AzureWork.DbModel;
+
+namespace AzureWork.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly DbModel.AdventureWorks2017Entities _entities = new DbModel.AdventureWorks2017Entities();
+
+        public IEnumerable<Product> Get()
+        {
+            var query = _entities.Products.Select(p => p);
+
+            return query.AsEnumerable();
+        }
+
+        public Product Get(int id)
+        {
+            var query = _entities.Products.FirstOrDefault(p => p.ProductID==id);
+
+            return query;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Put(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Post(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
